3a91cb7db0c1d8c288d5dba85fa2dcd3fcf1e8aa
Marcos Artimundo <51058488+martimundo@users.noreply.github.com>
2023-06-13 09:40:53 -0300

    Inicio do Projeto conhecendo o DOM e declarando variaveis através da querySelector

---
+++ index.js
@@ -0,0 +1,14 @@
+let txtNome     = document.querySelector("#exampleInputName");
+let txtGeneroM  = document.querySelector("#exampleInputGenderM");
+let txtGeneroF  = document.querySelector("#exampleInputGenderF");
+let dtNasc      = document.querySelector("#exampleInputBirth");
+let pais        = document.querySelector("#exampleInputCountry");
+let txtEmail    = document.querySelector("#exampleInputEmail1");
+let txtPasswd   = document.querySelector("#exampleInputPassword1");
+let uploadImage = document.querySelector("#exampleInputFile");
+
+
+txtNome.value  = "Marcos".toLocaleUpperCase();
+txtNome.style.color = "red";
+
+console.log(txtNome);
